4a85f787e679d99d7f2c71628aafe8be206bf7e2
eroscupid88 <68611769+eroscupid88@users.noreply.github.com>
2020-08-06 18:36:38 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,4 +12,5 @@ class profile::ssh_server {
     user     => 'root',
     type     => 'ssh-rsa',
     key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6bFChmbwCIGsfYMMUdXLweG6j5vSTtxazMJdKNAAsSyhTZz4BB0hmPG0FKhrTwKsFD4mGPPG8ocGE/+bvKJkcUUHxDv2IPJ2l0k9S2dywFsNK4RGiZ1eFwoUcYL8YfCWPVCpRKk/n1I77LHhdClA9UlUUm5IdYsoNpS1fqua4rS98ibEP6Y684mt3G0UcSWd3i/FVRSDO07qqJPnjU0bKk9ItvFmiOHNz7SLif59IkX3uz9R43jHS2Dn+it4MtOb0I8Ana0aAziw6L/xd+dp+saPSMK+FT6BcQ9tDxfGVX3qpUTiYj4CijCH/OrJAEWkJTTRES8cHIKCN7tt/Q3x3',
+  }
 }
